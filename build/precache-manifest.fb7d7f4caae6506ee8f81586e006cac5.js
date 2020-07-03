18677d1fec9208b557188deca46cf01714792834
KarthikBaskaran <karthikbaskaran555@gmail.com>
2020-07-03 23:37:36 +0530

    gitpage

--- build/precache-manifest.fb7d7f4caae6506ee8f81586e006cac5.js
+++ build/precache-manifest.fb7d7f4caae6506ee8f81586e006cac5.js
@@ -16,6 +16,10 @@ self.__precacheManifest = [
     "url": "/static/js/2.a6500ade.chunk.js"
   },
   {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
     "revision": "c720fb4f16d69cc3c5dba61bc96cdc0d",
     "url": "/static/media/arrow-left.c720fb4f.cur"
   },
@@ -24,16 +28,12 @@ self.__precacheManifest = [
     "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
   },
   {
-    "revision": "ef69e791ae57424b1901a28e2ad4d708",
-    "url": "/static/media/arrow-right.ef69e791.cur"
-  },
-  {
-    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
-    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
-  },
-  {
     "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
     "url": "/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "ef69e791ae57424b1901a28e2ad4d708",
+    "url": "/static/media/arrow-right.ef69e791.cur"
   },
   {
     "revision": "2de111dbed66694743b2c3edb23ad059",
