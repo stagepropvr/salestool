dbe34a90880f38085f4dc1bd40b2e07d4d1c6ebe
KarthikBaskaran <karthikbaskaran555@gmail.com>
2020-07-03 21:21:58 +0530

    build

---
+++ build/precache-manifest.427dd469b41b56967f40ed65b2864823.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "574245ce1d477ad31974",
+    "url": "/static/css/main.6bf56ff5.chunk.css"
+  },
+  {
+    "revision": "574245ce1d477ad31974",
+    "url": "/static/js/main.a90d100c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "240af0ac0b288a59926c",
+    "url": "/static/js/2.a6500ade.chunk.js"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "ef69e791ae57424b1901a28e2ad4d708",
+    "url": "/static/media/arrow-right.ef69e791.cur"
+  },
+  {
+    "revision": "c720fb4f16d69cc3c5dba61bc96cdc0d",
+    "url": "/static/media/arrow-left.c720fb4f.cur"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "2de111dbed66694743b2c3edb23ad059",
+    "url": "/index.html"
+  }
+];
