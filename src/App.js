ba8e6758f508ace881c57818de51140152bd714b
ghirihari <ghirihari@gmail.com>
2020-07-16 20:52:40 +0530

    Map Style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Salespage from './Layouts/Salespage'
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -8,15 +9,28 @@ import Dashboard from "./components/Dashboard";
 import Projects from "./components/Projects";
 import Createroom from "./components/Createroom";
 import Joinroom from "./components/Joinroom";
+import MapModal from "./components/Mapmodal"
+
 
 import { goToRoomInput } from './components/goToRoomInput';
 class App extends Component {
+
+  
+constructor(){
+  super();
+  this.state = {show:true}
+}
+
+  toggleModal = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+  }
+  
   render() {
     return (
       
        
         <div style={{width:"100%",background:'#fff'}}>
-          
+{/*           
       <Router >
        <Switch> 
         <Route  path='/login' component={Login} />
@@ -33,7 +47,12 @@ class App extends Component {
          <Route path='/createroom/:pid' component={Createroom} />
          <Route path='/room/:pid/:roomid' component={Salespage} />
          </Switch>
-         </Router>         
+         </Router>          */}
+
+         <MapModal
+        show={this.state.show}
+        onHide={() => this.toggleModal()}
+      />
   </div>
  
       
