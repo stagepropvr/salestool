0e3318fd061b7f61e466d05e42edaa4405844afd
KarthikBaskaran <karthikbaskaran555@gmail.com>
2020-08-10 20:29:19 +0530

    service worker

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = express();
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'salestool')));
 
 
-
+app.get('/salestool/service-worker.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'service-worker.js'));
+});
 
 app.get('/salestool/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- ssl.js
+++ ssl.js
@@ -10,14 +10,15 @@ const app = express();
 var path=require('path');
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'salestool')));
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+
+app.get('/salestool/service-worker.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'salestool', 'service-worker.js'));
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/salestool/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'salestool', 'index.html'));
 });
 
 
